5928c605839ff2b7590f2888dc221687a844c22f
FantasticFiasco <mattias.kindborg@gmail.com>
2021-02-11 22:41:32 +0100

    wip

---
+++ src/app/Options.cs
@@ -0,0 +1,13 @@
+using CommandLine;
+
+namespace App
+{
+    public class Options
+    {
+        [Option('n', "numbers", Default = 1000, HelpText = "The number of log events to write using Serilog")]
+        public int Numbers { get; set; }
+
+        [Option('d', "destination", Default = "http://localhost:8080", HelpText = "The URL of the log server")]
+        public string Destination { get; set; }
+    }
+}
--- src/app/Program.cs
+++ src/app/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using CommandLine;
+using Serilog;
 
-namespace app
+namespace App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+               .WithParsed(options => Run(options));
+        }
+
+        private static void Run(Options options)
+        {
+            ILogger log = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Http(requestUri: options.Destination)
+                .CreateLogger();
+
+            for (var i = 0; i < options.Numbers; i++)
+            {
+                log.Information("{@Date} Logging from app", DateTime.Now);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
