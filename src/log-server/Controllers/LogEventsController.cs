401d9663573c0dba5eb2fa6906cdb0bb1f828175
FantasticFiasco <mattias.kindborg@gmail.com>
2021-02-11 23:48:12 +0100

    wip

---
+++ src/log-server/Controllers/LogEventsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LogServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogEventsController : ControllerBase
+    {
+        private readonly ILogger<LogEventsController> logger;
+
+        public LogEventsController(ILogger<LogEventsController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] LogEvents body)
+        {
+            logger.LogInformation("Received batch of log events");
+        }
+    }
+
+    public class LogEvents
+    {
+        public LogEvent[] Events { get; set; }
+    }
+
+    public class LogEvent
+    {
+        //public DateTime Timestamp { get; set; }
+        public string Timestamp { get; set; }
+
+        public string Level { get; set; }
+
+        public string RenderedMessage { get; set; }
+    }
+}
