d57c616df8056fa156e00e2a76750f9be0d6ea10
FantasticFiasco <mattias.kindborg@gmail.com>
2021-02-15 07:39:27 +0100

    wip

--- src/log-server/Stats/StatisticsPrinter.cs
+++ src/log-server/Stats/StatisticsPrinter.cs
@@ -47,23 +47,23 @@ namespace LogServer.Stats
             var now = DateTime.Now;
 
             var messageBuilder = new StringBuilder();
-            messageBuilder.AppendLine(string.Format("Start:        {0:O}", statistics.Start));
-            messageBuilder.AppendLine(string.Format("Duration:     {0}", statistics.Start != null ? now.Subtract((DateTime)statistics.Start) : ""));
-            messageBuilder.AppendLine(string.Format("Batches:      {0}", statistics.BatchCount));
-            messageBuilder.AppendLine(string.Format("    /minute:  {0:N2}", statistics.BatchesPerMinute));
-            messageBuilder.AppendLine(string.Format("Events:       {0}", statistics.EventCount));
-            messageBuilder.AppendLine(string.Format("    /minute:  {0:N2}", statistics.EventsPerMinute));
+            messageBuilder.AppendLine("Start:        {0:O}".Format(statistics.Start));
+            messageBuilder.AppendLine("Duration:     {0}".Format(statistics.Start != null ? now.Subtract((DateTime)statistics.Start) : ""));
+            messageBuilder.AppendLine("Batches:      {0}".Format(statistics.BatchCount));
+            messageBuilder.AppendLine("    /minute:  {0:N2}".Format(statistics.BatchesPerMinute));
+            messageBuilder.AppendLine("Events:       {0}".Format(statistics.EventCount));
+            messageBuilder.AppendLine("    /minute:  {0:N2}".Format(statistics.EventsPerMinute));
             messageBuilder.AppendLine("Distribution:");
-            messageBuilder.AppendLine(string.Format("            size < 512B  : {0}", statistics.EventsOfSize(LogEventSize.Below512B)));
-            messageBuilder.AppendLine(string.Format("    512B  <= size < 1KB  : {0}", statistics.EventsOfSize(LogEventSize.Between512BAnd1KB)));
-            messageBuilder.AppendLine(string.Format("    1KB   <= size < 5KB  : {0}", statistics.EventsOfSize(LogEventSize.Between1And5KB)));
-            messageBuilder.AppendLine(string.Format("    5KB   <= size < 10KB : {0}", statistics.EventsOfSize(LogEventSize.Between5And10KB)));
-            messageBuilder.AppendLine(string.Format("    10KB  <= size < 50KB : {0}", statistics.EventsOfSize(LogEventSize.Between10And50KB)));
-            messageBuilder.AppendLine(string.Format("    50KB  <= size < 100KB: {0}", statistics.EventsOfSize(LogEventSize.Between50And100KB)));
-            messageBuilder.AppendLine(string.Format("    100KB <= size < 512KB: {0}", statistics.EventsOfSize(LogEventSize.Between100And512KB)));
-            messageBuilder.AppendLine(string.Format("    512KB <= size < 1MB  : {0}", statistics.EventsOfSize(LogEventSize.Between512KBAnd1MB)));
-            messageBuilder.AppendLine(string.Format("    1MB   <= size < 5MB  : {0}", statistics.EventsOfSize(LogEventSize.Between1And5MB)));
-            messageBuilder.AppendLine(string.Format("    5MB   <= size        : {0}", statistics.EventsOfSize(LogEventSize.Above5MB)));
+            messageBuilder.AppendLine("            size < 512B  : {0}".Format(statistics.EventsOfSize(LogEventSize.Below512B)));
+            messageBuilder.AppendLine("    512B  <= size < 1KB  : {0}".Format(statistics.EventsOfSize(LogEventSize.Between512BAnd1KB)));
+            messageBuilder.AppendLine("    1KB   <= size < 5KB  : {0}".Format(statistics.EventsOfSize(LogEventSize.Between1And5KB)));
+            messageBuilder.AppendLine("    5KB   <= size < 10KB : {0}".Format(statistics.EventsOfSize(LogEventSize.Between5And10KB)));
+            messageBuilder.AppendLine("    10KB  <= size < 50KB : {0}".Format(statistics.EventsOfSize(LogEventSize.Between10And50KB)));
+            messageBuilder.AppendLine("    50KB  <= size < 100KB: {0}".Format(statistics.EventsOfSize(LogEventSize.Between50And100KB)));
+            messageBuilder.AppendLine("    100KB <= size < 512KB: {0}".Format(statistics.EventsOfSize(LogEventSize.Between100And512KB)));
+            messageBuilder.AppendLine("    512KB <= size < 1MB  : {0}".Format(statistics.EventsOfSize(LogEventSize.Between512KBAnd1MB)));
+            messageBuilder.AppendLine("    1MB   <= size < 5MB  : {0}".Format(statistics.EventsOfSize(LogEventSize.Between1And5MB)));
+            messageBuilder.AppendLine("    5MB   <= size        : {0}".Format(statistics.EventsOfSize(LogEventSize.Above5MB)));
 
             logger.LogInformation(messageBuilder.ToString());
         }
