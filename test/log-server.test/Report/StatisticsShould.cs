627458dd1d936200543c061ac651c9e66f19e7db
FantasticFiasco <mattias.kindborg@gmail.com>
2021-02-21 08:54:59 +0100

    wip

--- test/log-server.test/Report/StatisticsShould.cs
+++ test/log-server.test/Report/StatisticsShould.cs
@@ -18,27 +18,27 @@ namespace LogServer.Report
         }
 
         [Fact]
-        public void ReturnNullStartGivenNoReportedBatches()
+        public void InitiallyBe()
         {
             // Assert
             statistics.Start.ShouldBeNull();
+            statistics.BatchCount.ShouldBe(0);
+            statistics.BatchesPerMinute.ShouldBeNull();
+            statistics.LogEventCount.ShouldBe(0);
+            statistics.LogEventsPerMinute.ShouldBeNull();
         }
 
         [Fact]
-        public void ReturnStartGivenReportedBatches()
+        public void ReturnStart()
         {
             // Arrange
-            statistics.ReportReceivedBatch(ByteSize.KB, new[] { ByteSize.KB });
+            statistics.ReportReceivedBatch(1, new long[1]);
+
+            // Act
+            var got = statistics.Start;
 
             // Assert
-            statistics.Start.ShouldNotBeNull();
-        }
-
-        [Fact]
-        public void ReturnZeroBatchCountGivenNoReportedBatches()
-        {
-            // Assert
-            statistics.BatchCount.ShouldBe(0);
+            got.ShouldNotBeNull();
         }
 
         [Theory]
@@ -46,16 +46,19 @@ namespace LogServer.Report
         [InlineData(2)]
         [InlineData(3)]
         [InlineData(99)]
-        public void ReturnBatchCountGivenReportedBatches(int batchCount)
+        public void ReturnBatchCount(int batchCount)
         {
             // Arrange
             for (var i = 0; i < batchCount; i++)
             {
-                statistics.ReportReceivedBatch(1, new long[] { 1 });
+                statistics.ReportReceivedBatch(1, new long[1]);
             }
 
+            // Act
+            var got = statistics.BatchCount;
+
             // Assert
-            statistics.BatchCount.ShouldBe(batchCount);
+            got.ShouldBe(batchCount);
         }
 
         [Theory]
@@ -66,7 +69,7 @@ namespace LogServer.Report
         [InlineData(1, 1, 1)]
         [InlineData(1, 2, 0.5)]
         [InlineData(10, 2, 5)]
-        public void ReturnBatchesPerMinute(int batchCount, double? minutes, double? expected)
+        public void ReturnBatchesPerMinute(int batchCount, double? minutes, double? want)
         {
             // Arrange
             var now = new DateTime(2020, 1, 1);
@@ -74,7 +77,7 @@ namespace LogServer.Report
 
             for (var i = 0; i < batchCount; i++)
             {
-                statistics.ReportReceivedBatch(1, new long[] { 1 });
+                statistics.ReportReceivedBatch(1, new long[1]);
             }
 
             if (minutes != null)
@@ -82,8 +85,62 @@ namespace LogServer.Report
                 statistics.Start = now.AddMinutes(-(double)minutes);
             }
 
+            // Act
+            var got = statistics.BatchesPerMinute;
+
             // Assert
-            statistics.BatchesPerMinute.ShouldBe(expected);
+            got.ShouldBe(want);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(1, 10, 10)]
+        [InlineData(2, 10, 20)]
+        [InlineData(4, 5, 20)]
+        public void ReturnLogEventCount(int batchCount, int logEventsPerBatch, int want)
+        {
+            // Arrange
+            for (var i = 0; i < batchCount; i++)
+            {
+                statistics.ReportReceivedBatch(1, new long[logEventsPerBatch]);
+            }
+
+            // Act
+            var got = statistics.LogEventCount;
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Theory]
+        [InlineData(0, null, null)]
+        [InlineData(1, 0.1, 10)]
+        [InlineData(2, 0.1, 20)]
+        [InlineData(10, 0.5, 20)]
+        [InlineData(1, 1, 1)]
+        [InlineData(1, 2, 0.5)]
+        [InlineData(10, 2, 5)]
+        public void ReturnLogEventsPerMinute(int logEvents, double? minutes, double? want)
+        {
+            // Arrange
+            var now = new DateTime(2020, 1, 1);
+            clock.Setup(mock => mock.Now).Returns(now);
+
+            if (logEvents > 0)
+            {
+                statistics.ReportReceivedBatch(1, new long[logEvents]);
+            }
+
+            if (minutes != null)
+            {
+                statistics.Start = now.AddMinutes(-(double)minutes);
+            }
+
+            // Act
+            var got = statistics.LogEventsPerMinute;
+
+            // Assert
+            got.ShouldBe(want);
         }
     }
 }
