0e98a9f3056ebf3ad8842c943731385780270e66
FantasticFiasco <mattias.kindborg@gmail.com>
2021-02-21 19:07:40 +0100

    wip

--- src/app/Options.cs
+++ src/app/Options.cs
@@ -4,7 +4,9 @@ namespace App
 {
     public class Options
     {
-        [Option('d', "destination", Default = "http://localhost:8080", HelpText = "The URL of the log server")]
+        [Option(
+            'd',
+            "destination", Default = "http://localhost:8080", HelpText = "The URL of the log server")]
         public string Destination { get; set; } = "http://localhost:8080";
 
         [Option('c', "concurrency", Default = 10, HelpText = "The number of concurrent tasks writing log events")]
@@ -13,7 +15,7 @@ namespace App
         [Option('r', "rate", Default = 1, HelpText = "The number of log events per second each task is writing")]
         public int Rate { get; set; } = 1;
 
-        [Option('m', "max-message-size", Default = 10, HelpText = "The maximum size, in KB, of a logged message")]
+        [Option('m', "max-message-size", Default = 10, HelpText = "The maximum size, in KB, of a logged message. Please note that this is a limit of the message size, not the serialized log event size.")]
         public int MaxSize { get; set; } = 10;
     }
 }
